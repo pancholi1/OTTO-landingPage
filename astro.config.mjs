be69908e08b9d9fdfa6f3b5ebf27ad66f45be10c
pancholi1 <francisco.olivero1998@gmail.com>
2026-05-04 00:59:20 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.ottoedu.com.ar",
+  devToolbar: {
+    enabled: false,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+});
